0015f879ccc7b6893bc612ef34f98e21a28240cb
SBala1398 <sombala1398@gmail.com>
2024-01-15 10:45:47 -0800

    final out put

--- product.js
+++ product.js
@@ -76,5 +76,88 @@ const products = [
         price:220,
       tags:["new","blue","summer"]
     },
-]
+    {
+        id: 10,
+        name: "Ocean Blue Shirt",
+        src: "products/f4.jpg",
+        desc: "",
+        price: 299,
+        tags: ["new", "blue", "beach"]
+    },
+
+    {
+        id: 11,
+        name: "Sunset Paradise Shirt",
+        src: "products/f2.jpg",
+        desc: "",
+        price: 349,
+        tags: ["new", "orange", "summer"]
+    },
+
+    {
+        id: 12,
+        name: "Tropical Vibes Shirt",
+        src: "products/f6.jpg",
+        desc: "",
+        price: 429,
+        tags: ["new", "green", "beach"]
+    },
+
+    // 6 New Objects
+
+    {
+        id: 13,
+        name: "Classic White Tee",
+        src: "products/f3.jpg",
+        desc: "",
+        price: 199,
+        tags: ["basic", "white", "casual"]
+    },
+
+    {
+        id: 14,
+        name: "Midnight Black Shirt",
+        src: "products/f5.jpg",
+        desc: "",
+        price: 259,
+        tags: ["classic", "black", "formal"]
+    },
+
+    {
+        id: 15,
+        name: "Sunny Yellow Polo",
+        src: "products/f4.jpg",
+        desc: "",
+        price: 319,
+        tags: ["bright", "yellow", "casual"]
+    },
+
+    {
+        id: 16,
+        name: "Denim Dream Jacket",
+        src: "products/f3.jpg",
+        desc: "",
+        price: 499,
+        tags: ["denim", "blue", "jacket"]
+    },
+
+    {
+        id: 17,
+        name: "Sporty Striped Hoodie",
+        src: "products/f2.jpg",
+        desc: "",
+        price: 379,
+        tags: ["sporty", "striped", "hoodie"]
+    },
+
+    {
+        id: 18,
+        name: "Boho Chic Dress",
+        src: "products/f1.jpg",
+        desc: "",
+        price: 589,
+        tags: ["boho", "chic", "dress"]
+    }
+];
+
 export {products}
